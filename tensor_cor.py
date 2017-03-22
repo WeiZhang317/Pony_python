2d0b3a723a5a2463f99c085f0572495d5cc34542
Wei Zhang <vivianzhang317@gmail.com>
2017-03-22 14:15:34 +0800

    ml tryout

--- tensor_cor.py
+++ tensor_cor.py
@@ -1,109 +1,124 @@
 import tensorflow as tf
 
 
-def tf_var_graph(x, n):
+def tf_var(x):
     x = x - tf.reduce_mean(x)
-    xx_sum = tf.reduce_sum(x * x)
-    x_sum = tf.reduce_sum(x)
-
-    var = (xx_sum - (x_sum * x_sum) / n) / (n - 1)
+    n = tf.to_float(tf.size(x))
+    var = tf.reduce_sum(x * x) / (n - 1)
     return var
 
 
-def tf_var(data,sess=None):
+def tf_var_run(data,sess=None):
     # todo check parameter
     x = tf.placeholder(tf.float32)
-    n = tf.placeholder(tf.float32)
-    f = tf_var_graph(x, n)
-    if not sess : return (f, {x: data, n: len(data)})
-    return sess.run(f, {x: data, n: len(data)})
+    f = tf_var(x)
+    if not sess : return (f, {x: data})
+    return sess.run(f, {x: data})
 
-def tf_cov_graph(x, y, n):
+def tf_cov(x, y):
+    n = tf.to_float(tf.size(x))
     x = x - tf.reduce_mean(x)
     y = y - tf.reduce_mean(y)
-
-    xy_sum = tf.reduce_sum(x * y)
-    x_sum = tf.reduce_sum(x)
-    y_sum = tf.reduce_sum(y)
-
-    return (xy_sum - (x_sum * y_sum) / n) / (n - 1)
+    cov = tf.reduce_sum(x * y) / (n - 1)
+    return cov
 
 
-def tf_cov(dataX, dataY,sess=None):
+def tf_cov_run(dataX, dataY,sess=None):
     # todo check parameter
     x = tf.placeholder(tf.float32)
     y = tf.placeholder(tf.float32)
-    n = tf.placeholder(tf.float32)
-    f = tf_cov_graph(x, y, n)
-
-    if not sess : return (f, {x: dataX, y: dataY, n: len(dataX)})
-    return sess.run(f, {x: dataX, y: dataY, n: len(dataX)})
+    f = tf_cov(x, y)
+    if not sess : return (f, {x: dataX, y: dataY})
+    return sess.run(f, {x: dataX, y: dataY})
 
 
-def tf_cor_graph(x, y, n):
+def tf_cor(x, y):
     # pearson
+    n = tf.to_float(tf.size(x))
     x = x - tf.reduce_mean(x)
     y = y - tf.reduce_mean(y)
 
-    xy_sum = tf.reduce_sum(x * y)
-    x_sum = tf.reduce_sum(x)
-    y_sum = tf.reduce_sum(y)
-
-    cov = (xy_sum - (x_sum * y_sum) / n) / (n - 1)
-
-    xx_sum = tf.reduce_sum(x * x)
-    x_var = (xx_sum - (x_sum * x_sum) / n) / (n - 1)
+    cov = tf.reduce_sum(x * y) / (n - 1)
+    x_var = tf.reduce_sum(x * x) / (n - 1)
     x_sd = tf.sqrt(x_var)
 
-    yy_sum = tf.reduce_sum(y * y)
-    y_var = (yy_sum - (y_sum * y_sum) / n) / (n - 1)
+    y_var = tf.reduce_sum(y * y)  / (n - 1)
     y_sd = tf.sqrt(y_var)
 
-    return cov / (x_sd * y_sd) # pearson
+    cor =  cov / (x_sd * y_sd)
+    return cor
 
-def tf_cor(dataX, dataY,sess=None):
+def tf_cor_run(dataX, dataY,sess=None):
     # pearson
     # todo check parameter
     x = tf.placeholder(tf.float32)
     y = tf.placeholder(tf.float32)
-    n = tf.placeholder(tf.float32)
-    f = tf_cor_graph(x, y, n)
-    if not sess : return (f, {x: dataX, y: dataY, n: len(dataX)})
-    return sess.run(f, {x: dataX, y: dataY, n: len(dataX)})
+    f = tf_cor(x, y)
+    if not sess : return (f, {x: dataX, y: dataY})
+    return sess.run(f, {x: dataX, y: dataY})
 
 def tf_cor_spearman(dataX, dataY):
     # The Spearman correlation between two variables is equal to the Pearson correlation between the rank values of those two variables;
     pass
 
 
-def tf_cor_kendall(dataX, dataY):
+def tf_cor_kendall(dataX, dataY,sess=None):
     pass
+
+def tf_cor_matrix(x):
+    # pearson
+    # todo check dim
+    n = tf.to_float(tf.shape(x)[1])
+    x_mean = tf.reduce_mean(x,1)
+    x = x - tf.expand_dims(x_mean,1)
+
+    x_var = tf.reduce_sum(x * x,1) / (n - 1)
+    x_sd = tf.sqrt(x_var)
+
+    def xix_cor(xi):
+        xix_cov = tf.reduce_sum(xi * x,1) / (n - 1)
+
+        xi_var =  tf.reduce_sum(xi * xi) / (n - 1)
+        xi_sd = tf.sqrt(xi_var)
+
+        xix_cor = xix_cov / (xi_sd*x_sd)
+        return xix_cor
+
+    result = tf.map_fn(xix_cor, x)
+    return result
+
+def tf_cor_matrix_run(data,sess=None):
+    # pearson
+    # todo check parameter
+    x = tf.placeholder(tf.float32)
+    f = tf_cor_matrix(x)
+    if not sess : return (f, {x: data})
+    return sess.run(f, {x: data})
+
 
 if __name__ == "__main__":
     sess = tf.Session()
-    f,feed = tf_var([1, 2, 3, 4, 5])
-    print sess.run(f,feed)
-    f,feed =  tf_cov([1, 2, 3, 4, 5], [1, 2, 3, 4, 5])
-    print sess.run(f,feed)
-    f,feed = tf_cor([1, 2, 3, 4, 5], [1, 2, 3, 4, 5])
-    print sess.run(f,feed)
+
+    print tf_var_run([1, 2, 3, 4, 5],sess)
+    print tf_cov_run([1, 2, 3, 4, 5], [1, 2, 3, 4, 5],sess)
+    print tf_cor_run([1, 2, 3, 4, 5], [1, 2, 3, 4, 5],sess)
+    print tf_cov_run([1, 3, 9, 8, 0], [2, 1, 3, 5, 9],sess)
+    print tf_cor_run([1, 3, 9, 8, 0], [2, 1, 3, 5, 9],sess)
     print
 
-    print tf_var([1, 2, 3, 4, 5],sess)
-    print tf_cov([1, 2, 3, 4, 5], [1, 2, 3, 4, 5],sess)
-    print tf_cor([1, 2, 3, 4, 5], [1, 2, 3, 4, 5],sess)
+    data = [[1, 3, 9], [8, 0, 1],[2, 1, 3], [5, 9, 0]]
+
+    for i in range(4) :
+        ret = []
+        for j in range(4) :
+            ret.append(tf_cor_run(data[i],data[j],sess))
+        print  ret
+
     print
-    print tf_var([1, 3, 9, 8, 0],sess)
-    print tf_cov([1, 3, 9, 8, 0], [1, 3, 9, 8, 0],sess)
-    print tf_cor([1, 3, 9, 8, 0], [1, 3, 9, 8, 0],sess)
-    print
-    print tf_var([1, 3, 9, 8, 0],sess)
-    print tf_cov([1, 3, 9, 8, 0], [2, 3, 8, 7, 1],sess)
-    print tf_cor([1, 3, 9, 8, 0], [2, 3, 8, 7, 1],sess)
-    print
-    print tf_var([1, 3, 9, 8, 0],sess)
-    print tf_cov([1, 3, 9, 8, 0], [2, 1, 3, 5, 9],sess)
-    print tf_cor([1, 3, 9, 8, 0], [2, 1, 3, 5, 9],sess)
+    print tf_cor_matrix_run(data,sess)
+    # todo use compensated summation(Kahan summation algorithm) to get more accuracy
+
+
 
 
 
